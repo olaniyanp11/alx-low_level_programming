df2f355641c4d5c3a66cb5c1a989a121b7318d43
olaniyanp11 <olaniyanp11@gmail.com>
2022-09-09 06:58:04 -0700

    nnn

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,11 +2,12 @@
 /**
  * main - enter
  *
- * return 0
- * */
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,")
+	printf("with proper grammar, but the outcome is a piece of art,");
 
-		return (0)
+		return (0);
 }
