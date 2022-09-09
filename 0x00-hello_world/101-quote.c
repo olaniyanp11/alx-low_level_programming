d1c8b8690749d45ab0ad0f381295db610a4e17a4
olaniyanp11 <olaniyanp11@gmail.com>
2022-09-09 07:31:24 -0700

    hhh

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - entrance
+ *
+ * Return : 0 (sucess)
+ *
+ **/
+
+int main(void)
+{
+
+	putchar("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+
+	return (1);
+}
