ed7692d3c6e0b9a5e7a945b16a88d708f6672dc9
olaniyanp11 <olaniyanp11@gmail.com>
2022-09-09 06:30:31 -0700

    gg

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+ #include <stdio.h>
+/**
+ * main -Entering function
+ * fst - to print out a text
+ *
+ * Return: Always 0 (Success)
+ **/
+int main(void)
+{
+
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
