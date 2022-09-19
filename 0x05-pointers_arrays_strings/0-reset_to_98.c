51d8c0fb713f94321257bf48f7211d279eaf27e5
olaniyanp11 <olaniyanp11@gmail.com>
2022-09-19 15:14:52 -0700

    kk

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * reset_to_98 -  takes a pointer to an int
+ * @n : is a pointer
+ * Return Always 0 (Success)
+ */
+void reset_to_98(int *n)
+{
+		int n;
+		int *bat;
+
+		n = 98;
+		n = &bat;
+		printf("%p\n", n);
+		return (0);
+}
