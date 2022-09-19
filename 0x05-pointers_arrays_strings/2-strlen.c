41219de41cc81e109f05062589e180bcdf61ae0b
olaniyanp11 <olaniyanp11@gmail.com>
2022-09-19 16:30:43 -0700

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - standard library provides a similar function
+ * Return: Always 0.
+ * @s : my string
+ */
+
+int _strlen(char *s)
+{
+	*s = "pointers";
+	return (0);
+}
