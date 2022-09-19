d7009cb9281e494e608c0f19c0abcba1faeb067d
olaniyanp11 <olaniyanp11@gmail.com>
2022-09-19 15:35:57 -0700

    task 2

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * swap_int - swaps the value of a and b
+ * @a :first pointer
+ * @b :second pointer
+ */
+
+void swap_int(int *a, int *b)
+{
+	a = 98;
+	b = 42;
+	swapnum(&a, &b);
+}
