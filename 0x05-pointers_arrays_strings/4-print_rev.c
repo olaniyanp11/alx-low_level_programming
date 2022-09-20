9a8bbe03c4bedcd7e7ca362ba8f7040bcfa66bc4
olaniyanp11 <olaniyanp11@gmail.com>
2022-09-19 17:30:36 -0700

    gg

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+
+
+/**
+ * print_rev - prints the reverse of a string.
+ * @s: input string.
+ * Return: nothing.
+*/
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i] != 0)
+	{
+		i++;
+	}
+	while (i > 0)
+	{
+		i--;
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
