ec91477b45c84999c792e15eff8d6aefd8694193
olaniyanp11 <olaniyanp11@gmail.com>
2022-09-30 18:04:49 -0700

    print number of lines

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main -print out number of passedf arguement
+ * @argc: counts the number of command line arguements
+ * @argv: arguemaent for checking size
+ * Return: 0 success
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc);
+	return (0);
+}
