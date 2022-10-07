01b27ed1b3af768eea046dd7eb1caa7717ea5493
olaniyanp11 <107733620+olaniyanp11@users.noreply.github.com>
2022-10-06 18:54:29 -0700

    Update 0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,1 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *malloc_checked - function that allocates memory using malloc
+ * @b: Unsigned int var
+ * Return: no return value
+*/
+void *malloc_checked(unsigned int b)
+{
+  char *g;
 
+	g = malloc(b);
+	if (p == NULL)
+	{
+		exit(98);
+	}
+	return (p);
+}
