0327ca5e48a3fc338ae4be639ebcf704b0201128
olaniyanp11 <107733620+olaniyanp11@users.noreply.github.com>
2022-10-07 11:17:28 -0700

    Create 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+*
+*/
+int main()
+{
+  _putchar('_');
+  _putchar('p');
+  _putchar('u');
+  _putchar('t');
+  _putchar('c');
+  _putchar('h');
+  _putchar('a');
+  _putchar('r');
+  _putchar('/n');
+  
+  return (0);
+}
