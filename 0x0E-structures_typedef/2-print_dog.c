d208514f75c34268db0c1b4c5c615b8da7e82212
olaniyanp11 <107733620+olaniyanp11@users.noreply.github.com>
2022-10-10 16:28:45 -0700

    Update 2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,8 +1,11 @@
+#include <stdio.h>
 #imclude <dog.h>
 /**
 * print_dog - function that prints a struct dog
-* @
-* 
+* @d : structdog
+* @name : name of the dog
+* @age : age of the dog
+* @owner : owner of the dog
 */
 void print_dog(struct dog *d)
 {
@@ -10,6 +13,7 @@ void print_dog(struct dog *d)
   float age;
   char *owner;
   
+  printf("%d",name);
   
 }
 
