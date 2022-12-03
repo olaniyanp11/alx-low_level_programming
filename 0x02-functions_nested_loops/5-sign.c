ae7e8648282c9f53a604925aa6355bb93cf6effd
olaniyanp11 <olaniyanp11@gmail.com>
2022-12-03 15:16:16 -0800

    task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
+#include "main.h"
 /**
  * print_sign - prints the sign of the any number passed
+ * _putchar - prints a single char
  * @n : used as a variable
  * Return: 0
  *
@@ -8,9 +10,18 @@
 int print_sign(int n)
 {
 	if (n > 0)
+	{
+		_putchar('+');
 		return (1);
+	}
 	else if (n < 0)
+	{
+		_putchar('-');
 		return (-1);
+	}
 	else
+	{
+		_putchar('0');
 		return (0);
+	}
 }
