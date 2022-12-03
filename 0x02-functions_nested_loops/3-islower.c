4d7a667da38c4c182ea28b8ecbb62089d5caa7f6
olaniyanp11 <olaniyanp11@gmail.com>
2022-12-03 14:01:23 -0800

    update
    git commit -m update

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,18 +1,13 @@
 #include <stdio.h>
-
 /**
- * _islower : to check if am alphabetis lower case.
- * _islower return : 0 success
- * @c : any variable inputted by the user
- */
+ * _islower - return 1 with lower case and 0 with upper case
+ * @c : a variable
+ * Return: 0 success
+*/
 int _islower(int c)
 {
-	if (c > 96 && c < 123)
-	{
+	if (c > 96 && c < 122)
 		return (1);
-	}
 	else
-	{
 		return (0);
-	}
 }
--- README.md
+++ README.md
@@ -1,3 +1,2 @@
-###A repository for functions and nested loops
-##number 2: a file that checks if an alphabet is lower case
-
+<H1>&diams; A repository for functions and nested loops</H1>
+<H3> &diams; number 2: a file that checks if an alphabet is lower case</H3>
