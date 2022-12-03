49bdfbacfed082acea87481ec1327761257ca789
olaniyanp11 <olaniyanp11@gmail.com>
2022-12-03 14:26:12 -0800

    task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * _isalpha - checks if c isan alphabet
+ * @c : is a variable  used to check
+ * Return: 0
+ */
+int _isalpha(int c)
+{
+	if (c > 96 && c < 123)
+	{
+		return (1);
+	}
+	else if (c > 64 && c < 91)
+		return (1);
+	else 
+		return (0);
+}
+
