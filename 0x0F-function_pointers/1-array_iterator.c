07862ca4fe538ed27e2b6e80e5b9718b525a24a6
olaniyanp11 <olaniyanp11@gmail.com>
2023-01-20 15:41:03 +0100

    task 1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -13,7 +13,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 	size_t i;
 	int j = 0;
 
-	if (size && array)
+	if (size && array && action)
 	{
 		for (i = 0; i < size; i++)
 		{
