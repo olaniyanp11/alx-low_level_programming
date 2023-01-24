f86c065b4063ff567ca1ca281c60941b05d9e0ed
olaniyanp11 <olaniyanp11@gmail.com>
2023-01-24 08:43:08 +0100

    task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,26 +8,39 @@
 void times_table(void)
 {
 	int i, j;
+	char comma = ',', space = ' ';
 
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j < 10; j++)
 		{
+			int p = i * j;
+
 			if (j == 9)
 			{
-				printf("%3d", i * j);
-			}
-			else if ((i * j) >= 10)
-			{
-				printf("%3d,", i * j);
+				if (p < 10)
+				{
+					_putchar(space);
+					_putchar((p % 10) + ('0' - 0));
+				}
+				else
+				{
+					_putchar((p / 10) + ('0' - 0));
+					_putchar((p % 10) + ('0' - 0));
+				}
 			}
 			else if (j == 0)
 			{
-				printf("%d,", i * j);
+				_putchar(p + ('0' - 0));
+				_putchar(comma);
+				_putchar(space);
 			}
 			else
 			{
-				printf("%3d,", i * j);
+				_putchar((p / 10) + ('0' - 0));
+				_putchar((p % 10) + ('0' - 0));
+				_putchar(comma);
+				_putchar(space);
 			}
 		}
 		printf("\n");
