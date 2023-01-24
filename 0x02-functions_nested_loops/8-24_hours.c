908c8c7cab2214ff7560a2a3fedd85b4adee6ac5
olaniyanp11 <olaniyanp11@gmail.com>
2023-01-24 08:46:31 +0100

    task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -13,7 +13,13 @@ void jack_bauer(void)
 	{
 		for (j = 0; j < 60; j++)
 		{
-			printf("%02d:%02d\n", i, j);
+			
+			_putchar((i / 10) + ('0' - 0));
+			_putchar((i % 10) + ('0' - 0));
+			_putchar(':');
+			_putchar((j / 10) + ('0' - 0));
+			_putchar((j % 10) + ('0' - 0));
+			_putchar('\n');
 		}
 	}
 }
