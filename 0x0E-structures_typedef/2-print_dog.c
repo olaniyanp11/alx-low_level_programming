b9ea43213d8938d1686a5314343cb34333979577
olaniyanp11 <olaniyanp11@gmail.com>
2023-01-25 10:30:09 +0100

    simplified code

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,12 +1,15 @@
 #include "dog.h"
-
+/**
+ * print_dog - prints the elements instruct dog
+ * @d: a pointer to the stucture
+ */
 void print_dog(struct dog *d)
 {
 	if (d)
 	{
-		printf("Name: %s\n",d->name);
-		printf("age: %f\n",d->age);
-		printf("owner: %s\n",d->owner);
+		printf("Name: %s\n", d->name);
+		printf("age: %f\n", d->age);
+		printf("owner: %s\n", d->owner);
 	}
 	else
 	{
