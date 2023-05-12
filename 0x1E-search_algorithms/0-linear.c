56d0539399214b5a56b16bf886ca74b88ce1972a
olaniyanp11 <olaniyanp11@gmail.com>
2023-05-12 19:52:12 +0000

    trying to get past betty coding style

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -13,12 +13,10 @@ int linear_search(int *array, size_t size, int value)
 {
 	size_t i = 0;
 
-	/**
-	 * if (array == NULL)
+	if (array == NULL)
 	{
 		return (-1);
 	}
-	*/
 	for (i = 0; i < size; i++)
 	{
 		printf("Value checked array[%li] = [%d]\n", i, array[i]);
